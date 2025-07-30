80130b3b782fa952d2e8e771f3b735aee3951d1a
Srivallika-Yadla040706 <yadla.srivallika2006@gmail.com>
2025-07-30 13:48:28 +0530

    adding all files

---
+++ activity1.py
@@ -0,0 +1,6 @@
+#print either number is even or odd number
+number=int(input("enter your number"))
+if (number%2==0):
+    print("the number is even number")
+else:
+    print("the number is odd number")
---
+++ activity5.py
@@ -0,0 +1,13 @@
+number=10
+scores=[10,20,30]
+names=["Srivalli","Jyothi","veeru"]
+for i in range (number):
+    print(i)
+
+
+for score in scores:    
+    print(score)
+
+
+for name in names:
+    print(name)    
